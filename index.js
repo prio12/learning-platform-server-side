713c17e1c71ea26aa9f0e810fefd0f592c8b66c9
MRPrio <maksudurprio.mp@gmail.com>
2022-10-26 00:01:40 +0600

    necessary api created

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors")
 
-const homeData = require("./Data/homeData.json")
+const homeData = require("./Data/homeData.json");
+const categories = require("./Data/categories.json");
+const courses = require("./Data/courses.json");
 
 
 app.use(cors())
@@ -17,6 +19,20 @@ app.get('/',(req,res) =>{
 app.get('/homeData', (req,res) =>{
     res.send(homeData)
 })
+
+app.get('/courses-categories', (req,res) =>{
+    res.send(categories)
+})
+
+app.get('/courses', (req, res) =>{
+    res.send(courses)
+})
+
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find((course) => course._id === id);
+    res.send(selectedCourse);
+  });
 
 app.listen(port, () =>{
     console.log('Server is running on port', port);
